252f773c24e935e01281ac6c1a2487cce8a1504e
Ibrahim A Yonees <ibrahim21383@gmail.com>
2018-08-10 16:09:05 +0200

    my_array_count_values added

---
+++ my_array_count_values.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * count every repeated value in the given array
+ *
+ * @param array $array
+ * @return array
+ */
+function my_array_count_values(array $array):array
+{
+    $array_counted_values = [];
+    foreach($array as $item){
+            if(gettype($item) !== 'string' && gettype($item) !== 'integer'){
+                    trigger_error('my_array_count_values(): Can only count STRING and INTEGER values!',E_USER_WARNING);
+                    continue;
+            }
+            if(!isset($array_counted_values[$item])){
+                    $array_counted_values[$item] = 1;
+            }else{
+                    $array_counted_values[$item]++;
+            }            
+    }
+    return $array_counted_values;
+}
+//tests for the function
+print_r(my_array_count_values([1,'test',1,'test',[],null,1.5,'test'])); //with warnings
+print_r(my_array_count_values([1,'test',1,'test','test'])); //without warnings
+
